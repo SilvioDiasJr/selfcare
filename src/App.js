d923d240aed18741756df8b25784cb010009e913
Silvio <silvio.dias@etec.sp.gov.br>
2021-01-28 00:10:46 -0300

    adicionar componentes e estilização global

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import LandingPage from './LandingPage'
+import GlobalStyle from './styles/global'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <Header/>
+      <LandingPage/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
